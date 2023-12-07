264df16a04d3a5612e72d7163f9cca5b041543bb
StringiPicasso <104991228+StringiPicasso@users.noreply.github.com>
2023-12-07 22:34:22 +0300

    Update IExcelService.cs

--- ExcelTablesInConsole/ExcelTablesInConsole/IExcelService.cs
+++ ExcelTablesInConsole/ExcelTablesInConsole/IExcelService.cs
@@ -4,9 +4,9 @@ namespace ExcelTablesInConsole
 {
     internal interface IExcelService
     {
-        public string productsWorksheet { get; }
-        public string entryWorksheet { get; }
-        public string customersWorksheet { get; }
+        public string ProductsWorksheet { get; }
+        public string EntryWorksheet { get; }
+        public string CustomersWorksheet { get; }
 
         public void SearchCustomersByProductName(XLWorkbook workbook, string productName);
         public void EditCustomerContact(XLWorkbook workbook, string customerName, string newContact, string newCompany);
